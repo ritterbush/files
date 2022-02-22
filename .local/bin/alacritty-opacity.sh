e5e6bef1de9da55ca443819dd06466af2b3281be
Paul Ritterbush <ritterbush@berkeley.edu>
2022-02-22 09:48:21 -0800

    alacritty 0.10 support

--- .local/bin/alacritty-opacity.sh
+++ .local/bin/alacritty-opacity.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-
-alacrittyrc_path=~/.config/alacritty/alacritty.yml 
+alacrittyrc_path=~/.config/alacritty/alacritty.yml
 
 #sed -i "s/background_opacity.*/background_opacity: 0.$1/" $alacrittyrc_path
 #Check arg's an integer
@@ -23,15 +22,13 @@ fi
 #[ ${#1} -eq 1 ] && echo YES ; sed -i "s/background_opacity.*/background_opacity: 0.0$1/" $alacrittyrc_path
 
 
-
 if [ ${#1} -eq 1 ]
     then
-        sed -i "s/background_opacity.*/background_opacity: 0.0$1/" $alacrittyrc_path
+        sed -i "s/opacity:.*/opacity: 0.0$1/" $alacrittyrc_path
 elif [ ${#1} -eq 2 ]
     then
-       sed -i "s/background_opacity.*/background_opacity: 0.$1/" $alacrittyrc_path
-
+       sed -i "s/opacity:.*/opacity: 0.$1/" $alacrittyrc_path
 elif [ ${#1} -eq 3 ]
     then
-       sed -i "s/background_opacity.*/background_opacity: 1.0/" $alacrittyrc_path
+       sed -i "s/opacity:.*/opacity: 1.0/" $alacrittyrc_path
 fi
