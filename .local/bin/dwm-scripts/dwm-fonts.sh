7a6c351318d3a7bffe818cee6c5bea1bf83e1288
ritterbush <ritterbush@berkeley.edu>
2022-07-24 20:20:35 -0700

    dwm fonts script reworked; no sleep delay when starting; date appears on startup; dwm error logging

--- .local/bin/dwm-scripts/dwm-fonts.sh
+++ .local/bin/dwm-scripts/dwm-fonts.sh
@@ -1,48 +1,80 @@
-#! /bin/sh
+#!/bin/sh
 
-#Options: -f [font name] -s [size number] -a [enables antialiasing] -h [enables auto hinting--makes font look better]
+show_usage(){
+    printf "Usage:\n\n  %s [options [parameters]]\n" "$0"
+    printf "\n"
+    printf "Easily gives dwm and dmenu a new font.\n"
+    printf "\n"
+    printf "Use \"fc-list\" on the command line to see available fonts; use the name
+on the right side. If there are spaces, encapsulate it in quotes.\n"
+    printf "\n"
+    printf "Options [parameters]:\n"
+    printf "\n"
+    printf "  -f [font]   Specify font name.\n"
+    printf "  -s [size]   Specify font size.\n"
+    printf "  -aa         Use antialiasing.\n"
+    printf "  -ah         Use autohinting. Makes font look better. \n"
+    printf "  --default   Use the best: Linux Libertine:style=Bold Italic
+                              with -aa and -ah.\n"
+    printf "  -h|--help   Print this help.\n"
+exit
+}
 
-#Use fc-list on the command line to see available fonts; use the name on the right. If there are spaces, encapsulate it in quotes.
+font="Linux Libertine:style=Bold Italic"
+size=":size=10"
+antialias=""
+autohint=""
 
-#Seds new font value to dwm after converting it into its equivalent hex and then rebuilds dwm.
+[ $# -eq 0 ] && show_usage
 
-#Check that argument is a number
-#[[ $1 =~ ^-?[0-9]+$ ]] &&
-
-size=':size=10'
-antialias=''
-autohint=''
-
-while getopts ":f:s:ah" opt; do
-  case ${opt} in
-    f ) font=${OPTARG}
-      ;;
-    s ) size='size=':${OPTARG}
-      ;;
-    a ) antialias=':antialias=true'
-      ;;
-    h ) autohint=':autohint=true'
-      ;;
-  esac
+while [ -n "$1" ]; do
+    case "$1" in
+        -f)
+            if [ -n "$2" ]
+            then
+                font="$2"
+                shift 2
+            else
+                echo "-f flag requires a font. Use -h to see cmd to get fonts."
+                exit
+            fi
+            ;;
+        -s)
+            if [ -n "$2" ]
+            then
+                [ -z "${2##*[!0-9]*}" ] && { echo "Size requires a number."; exit; } # Check $2 is a number
+                size=":size=$2"
+                shift 2
+            else
+                echo "-s flag requires a font size."
+                exit
+            fi
+            ;;
+        -aa)
+            antialias=":antialias=true"
+            shift
+            ;;
+        -ah)
+            autohint=":autohint=true"
+            shift
+            ;;
+        --help|-h)
+            show_usage
+            ;;
+        *)
+            echo "Unknown option $1"
+            show_usage
+            ;;
+    esac
 done
 
-echo $font
-echo $size
-echo $antialias
-echo $autohint
+font="${font}${size}${antialias}${autohint}"
 
-font=${font}${size}${antialias}${autohint}
-
-echo $font
-
+# Put new font values into dwm (applies to dmenu too)
 sed -i "s/^static const char \*fonts\[\]          = { \".*/static const char *fonts[]          = { \"$font\" };/" ~/Programs/dwm/config.def.h
 sed -i "s/^static const char dmenufont\[\]       = \".*/static const char dmenufont[]       = \"$font\";/" ~/Programs/dwm/config.def.h
 
+# Rebuild dwm with font changes (applies to dmenu too)
+([ -f ~/Programs/dwm/config.h ] && rm -f ~/Programs/dwm/config.h  && echo "Deleted old config.h, rebuilding dwm with font value: $font" && cd ~/Programs/dwm/ && sudo make clean install) ||
 
-#Rebuild dwm with font changes
-    ([ -f ~/Programs/dwm/config.h ] && rm -f ~/Programs/dwm/config.h  && echo 'Deleted old config.h, rebuilding dwm with new fonts' && cd ~/Programs/dwm/ && sudo make clean install) ||
-
-    ( echo 'Rebuilding dwm with new fonts' && cd ~/Programs/dwm/ && sudo make clean install)
-
-
-
+    ( echo "Rebuilding dwm with font value: $font" && cd ~/Programs/dwm/ && sudo make clean install)
