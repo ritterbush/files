458287bb698e9520a0c9d9c9b112a92bd658d709
ritterbush <ritterbush@berkeley.edu>
2022-07-28 07:47:14 -0700

    specify sleep seconds

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -8,6 +8,6 @@ delay=0
 [ $# -eq 0 ] && delay=3 # Default value if no args
 [ -n "$1" ] && [ -z "${1##*[!0-9]*}" ] && delay=$1 # if $1 exists and is a number
 
-sleep $delay
+sleep ${delay}s
 
 import -window root "$name".png
