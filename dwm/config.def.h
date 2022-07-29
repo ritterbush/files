40177d2965d3c0ad92a97a86e5ac2170edaaed93
ritterbush <ritterbush@berkeley.edu>
2022-07-29 14:07:02 -0700

    dwm urgentboarder patch

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -17,6 +17,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_urgborder[]   = "#ff0000";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 /* py-wal colorscheme */
